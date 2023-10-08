14985f314106fbbeade7b3314dc2fd7bc07a78cb
jreyes1990 <jarl1990mt@gmail.com>
2023-10-07 19:55:00 -0600

    108. Crear las especificaciones de un paquetes

---
+++ ORACLE/PLSQL/SECCION12.sql
@@ -0,0 +1,7 @@
+/******************************************************************************/
+/***** PACKAGES DE PL/SQL *****/
+CREATE OR REPLACE PACKAGE PACK1
+IS
+  V1 NUMBER;
+  V2 VARCHAR2(100);
+END;

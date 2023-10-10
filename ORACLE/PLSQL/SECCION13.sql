31b19e9dd901daa6434ece1fe8d89a2c6af76fec
jreyes1990 <jarl1990mt@gmail.com>
2023-10-09 21:16:09 -0600

    118. Crear un trigger

--- ORACLE/PLSQL/SECCION13.sql
+++ ORACLE/PLSQL/SECCION13.sql
@@ -15,3 +15,27 @@
       - STATEMENT: Solo se disparan una vez (afecta a toda la operacion), EJEMPLO: DELETE de 1000 filas (orientado a controlar el comando completo).
       - ROW: Se dispara por cada fila afectada, EJEMPLO: DELETE de 1000 filas, el TRIGGER se dispararia 1000 veces (oriendado a controlar cada fila).
 */
+
+/***** CREAR UN TRIGGER DE PL/SQL *****/
+-- CREANDO LA TABLA LOG_TABLE
+CREATE TABLE LOG_TABLE(
+  LOG_COLUMN  VARCHAR2(200 BYTE),
+  USER_NAME   VARCHAR2(200 BYTE)
+);
+
+CREATE OR REPLACE TRIGGER TR_INS_EMPL
+AFTER INSERT ON REGIONS --Se diparara despues de la operacion
+BEGIN
+  INSERT INTO LOG_TABLE VALUES('INSERCION EN LA TABLA REGIONS', USER);
+END;
+
+-- REALIZANDO UN INSERT EN LA TABLA REGIONS
+INSERT INTO REGIONS VALUES(1000,'TEST_REGIONS');
+
+-- EJECUTANDO EL LOG_TABLE
+SELECT * FROM LOG_TABLE;
+
+
+
+
+

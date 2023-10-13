6b4f1a39519341c0340d2c182b01070568715fc3
jreyes1990 <jarl1990mt@gmail.com>
2023-10-12 22:09:47 -0600

    16. Crear un REF CURSOR

---
+++ ORACLE/PLSQL - Avanzado/SECCION3.sql
@@ -0,0 +1,10 @@
+/******************************************************************************/
+/***** CREAR UN REF CURSOR *****/
+SET SERVEROUTPUT ON;
+
+DECLARE
+  TYPE CURSOR_VARIABLE IS REF CURSOR;
+  V1 CURSOR_VARIABLE;
+BEGIN
+  
+END;

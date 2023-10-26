0f0e99bb7fa290ef3e2bec6625bc9419d8fdade1
jreyes1990 <jarl1990mt@gmail.com>
2023-10-25 23:06:55 -0600

    44. Literales

--- ORACLE/SQL/SECCION4.sql
+++ ORACLE/SQL/SECCION4.sql
@@ -1,3 +1,9 @@
 /******************************************************************************/
 /* PRIMEROS PASOS */
 select first_name, last_name, salary as "Salario Empleado" from employees;
+
+
+/* LITERALES */
+select 'NOMBRE: ',first_name from employees;
+
+select 'NOMBRE: '||first_name||' '||last_name as "Nombre Empleado" from employees;

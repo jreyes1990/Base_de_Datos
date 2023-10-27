cf3a30197d17c6fbe8ff54e66392175b832b2337
jreyes1990 <jarl1990mt@gmail.com>
2023-10-26 22:28:03 -0600

    47. Nulos-NULL

--- ORACLE/SQL/SECCION4.sql
+++ ORACLE/SQL/SECCION4.sql
@@ -7,3 +7,7 @@ select first_name, last_name, salary as "Salario Empleado" from employees;
 select 'NOMBRE: ',first_name from employees;
 
 select 'NOMBRE: '||first_name||' '||last_name as "Nombre Empleado" from employees;
+
+
+/* NULOS */
+select first_name, salary, commission_pct, salary*commission_pct as "SALARIO TOTAL" from employees;

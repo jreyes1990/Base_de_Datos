90e896b0555de62cbdedaadae416eddcaf982144
jreyes1990 <jarl1990mt@gmail.com>
2023-10-26 22:38:21 -0600

    48. DISTINCT

--- ORACLE/SQL/SECCION4.sql
+++ ORACLE/SQL/SECCION4.sql
@@ -11,3 +11,9 @@ select 'NOMBRE: '||first_name||' '||last_name as "Nombre Empleado" from employee
 
 /* NULOS */
 select first_name, salary, commission_pct, salary*commission_pct as "SALARIO TOTAL" from employees;
+
+
+/* DISTINCT */
+select distinct department_id from employees;
+
+select distinct department_id, job_id from employees;
